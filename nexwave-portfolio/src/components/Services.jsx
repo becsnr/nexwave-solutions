4d1ad2c02b73b8a5b60e9328022bf081c79ae8ef
Beca <234265987+becsnr@users.noreply.github.com>
2026-05-13 14:43:30 -0300

    cards

--- nexwave-portfolio/src/components/Services.jsx
+++ nexwave-portfolio/src/components/Services.jsx
@@ -1,41 +1,83 @@
-import styles from './Services.module.css'
+import styles from './Services.module.css';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import web from '../assets/icons/web-icone.png';
+import ui from '../assets/icons/ui-icone.png';
+import app from '../assets/icons/app-icone.png';
+import cyber from '../assets/icons/cyber-icone.png';
 
 function Services() {
     return (
         <section className={styles.services}>
-            <p>Serviços</p>
+            <p>Nossos serviços</p>
 
-            <div className={styles.cards}>
-                <img src="" alt="" />
+            <Swiper 
+                className={styles.cards}
+                modules={[Autoplay, Pagination]}
+                spaceBetween={20}
+                slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false
+                }}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    768: {
+                        slidesPerView: 2
+                    },
 
-                <h2>Desenvolvimento Web</h2>
+                    1024: {
+                        slidesPerView: 4
+                    }
+                }}
+            >
+                <SwiperSlide>
+                    <div className={styles.card}>
+                        <img src={web} alt="" />
 
-                <h3>Site, plataformas e sistemas personalizados com tecnologia de ponta.</h3>
-            </div>
+                        <h1>Desenvolvimento Web</h1>
 
-            <div className={styles.cards}>
-                <img src="" alt="" />
+                        <h2>Site, plataformas e sistemas personalizados com tecnologia de ponta.</h2>
+                    </div>
+                </SwiperSlide>
 
-                <h2>UI/UX Design</h2>
+                <SwiperSlide>
+                    <div className={styles.card}>
+                        <img src={ui} alt="" />
 
-                <h3>Experiências intuitivas e interfaces que encantam e convertem.</h3>
-            </div>
+                        <h1>UI/UX Design</h1>
 
-            <div className={styles.cards}>
-                <img src="" alt="" />
+                        <h2>Experiências intuitivas e interfaces que encantam e convertem.</h2>
+                    </div>
+                </SwiperSlide>
 
-                <h2>Apps Mobile</h2>
+                <SwiperSlide>
+                    <div className={styles.card}>
+                        <img src={app} alt="" />
+                    
+                        <h1>Apps Mobile</h1>
 
-                <h3>Aplicativos nativos e hibridos para iOS e Android com alta perfomance.</h3>
-            </div>
+                        <h2>Aplicativos nativos e hibridos para iOS e Android com alta perfomance.</h2>
+                    </div>
+                </SwiperSlide>
+                
+                <SwiperSlide>
+                    <div className={styles.card}>
+                        <img src={cyber} alt="" />
+                    
+                        <h1>Cyber Segurança</h1>
 
-            <div className={styles.cards}>
-                <img src="" alt="" />
-
-                <h2>Cyber Segurança</h2>
-
-                <h3></h3>
-            </div>
+                        <h2>Proteção para sistemas, dados e infraestruturas contra ameaças e vulnerabilidades.</h2>
+                    </div>
+                </SwiperSlide>
+                
+            </Swiper>
         </section>
     )
 }
